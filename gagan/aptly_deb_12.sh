1fadee42fe932378d5b5ca66e1590b6f5c990312
gagan <gsingh@alertdriving.com>
2024-11-08 18:20:36 -0500

    initial commit :

--- gagan/aptly_deb_12.sh
+++ gagan/aptly_deb_12.sh
@@ -11,12 +11,13 @@ check_mirror() {
 }
 
 # Create mirrors only if they don't exist
+
 check_mirror "bookworm-main" "aptly mirror create bookworm-main http://deb.debian.org/debian bookworm main"
-check_mirror "bookworm-security" "aptly mirror create bookworm-security http://security.debian.org/debian-security bookworm-security main"
+check_mirror "bookworm-security" "aptly mirror create -force-components bookworm-security http://security.debian.org/debian-security bookworm-security main"
 check_mirror "bookworm-updates" "aptly mirror create bookworm-updates http://deb.debian.org/debian bookworm-updates main"
 check_mirror "percona-bookworm-main" "aptly mirror create percona-bookworm-main http://repo.percona.com/apt bookworm main"
 check_mirror "percona-prel-bookworm-main" "aptly mirror create percona-prel-bookworm-main http://repo.percona.com/prel/apt bookworm main"
-check_mirror "docker-bookworm-stable" "aptly mirror create docker-bookworm-stable https://download.docker.com/linux/debian bookworm stable"
+check_mirror "docker-bookworm-stable" "aptly mirror create -architectures=amd64,arm64,armhf docker-bookworm-stable https://download.docker.com/linux/debian bookworm stable"
 check_mirror "php-bookworm-main" "aptly mirror create php-bookworm-main https://packages.sury.org/php/ bookworm main"
 check_mirror "azul-repo-java-stable" "aptly mirror create azul-repo-java-stable https://repos.azul.com/zulu/deb/ stable main"
 
@@ -63,7 +64,4 @@ fi
 
 
 
-##### run with no hup : nohup ./aptly_deb_12.sh > aptly_log.out 2>&1 &
-
-
-####check logs : tail -f /root/gagan/aptly_log.out
+##### run with no hup : nohup ./aptly_deb_12.sh > aptly_log.out 2>&1 &   ; tail -f /root/gagan/aptly_log.out
